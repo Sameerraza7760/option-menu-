a28c8f55ee2e0fd68f5430ec125d6326a21e209c
Sameer Raza <146120654+Sameerraza7760@users.noreply.github.com>
2023-10-12 05:47:11 -0700

    Add files via upload

---
+++ option/app.js
@@ -0,0 +1,21 @@
+// var select=document.getElementById('select')
+// for(i=0;i<30;i++){
+//      var option= document.createElement('option')
+//      option.innerHTML=[i]
+//      select.appendChild(option)
+
+// }
+
+
+// var data =1
+// var data ="sameer"
+
+//    function enter(){
+//     console.log(data)
+//     //   addtodo(data)
+// } 
+// enter(data)
+// // function addtodo(){
+   
+// // }
+
